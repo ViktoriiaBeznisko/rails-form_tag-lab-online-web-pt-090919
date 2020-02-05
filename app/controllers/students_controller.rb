b0bc966e0fa108e313973d9eb7f2fb0ac00df063
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2020-02-05 00:45:55 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -17,4 +17,9 @@ class StudentsController < ApplicationController
     redirect_to student_path
   end
   
+  def create
+    Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+    redirect_to students_path
+  end
+  
   end
